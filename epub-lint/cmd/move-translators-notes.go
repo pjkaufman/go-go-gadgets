510bc09f63f3992528609ec2d2a5e9a6aaec5e9a
Peter Kaufman <PeterJamesKaufman@gmail.com>
2025-08-23 16:26:02 -0400

    added an example and better docs for usage as well as a simpler command name

--- epub-lint/cmd/move-translators-notes.go
+++ epub-lint/cmd/move-translators-notes.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/MakeNowJust/heredoc"
 	epubhandler "github.com/pjkaufman/go-go-gadgets/epub-lint/internal/epub-handler"
 	"github.com/pjkaufman/go-go-gadgets/epub-lint/internal/linter"
 	filehandler "github.com/pjkaufman/go-go-gadgets/pkg/file-handler"
@@ -29,48 +30,15 @@ var defaultTLNoteContents = `<?xml version='1.0' encoding='utf-8'?>
 
 // translatorsNotesCmd represents the move translator's notes command
 var translatorsNotesCmd = &cobra.Command{
-	Use:   "tl_notes",
+	Use:   "notes",
 	Short: "Moves translator's notes to their own file at the end of the epub.",
-	// Example: heredoc.Doc(`To run all of the possible potential fixes:
-	// epub-lint fixable -f test.epub -a
-	// Note: this will require a css file to already exist in the epub
-
-	// To just fix broken paragraph endings:
-	// epub-lint fixable -f test.epub --broken-lines
-
-	// To just update section breaks:
-	// epub-lint fixable -f test.epub --section-breaks
-	// Note: this will require a css file to already exist in the epub
-
-	// To just update page breaks:
-	// epub-lint fixable -f test.epub --page-breaks
-	// Note: this will require a css file to already exist in the epub
-
-	// To just fix missing oxford commas:
-	// epub-lint fixable -f test.epub --oxford-commas
-
-	// To just fix potentially lacking subordinate clause instances:
-	// epub-lint fixable -f test.epub --lacking-subordinate-clause
-
-	// To just fix instances of thoughts in parentheses:
-	// epub-lint fixable -f test.epub --thoughts
-
-	// To run a combination of options:
-	// epub-lint fixable -f test.epub -oxford-commas --thoughts --necessary-words
-	// `),
-	// Long: heredoc.Doc(`Goes through all of the content files and runs the specified fixable actions on them asking
-	// for user input on each value found that matches the potential fix criteria.
-	// Potential things that can be fixed:
-	// - Broken paragraph endings
-	// - Section breaks being hardcoded instead of an hr
-	// - Page breaks being hardcoded instead of an hr
-	// - Oxford commas being missing before or's or and's
-	// - Possible instances of sentences with two subordinate clauses (i.e. have although..., but)
-	// - Possible instances of thoughts that are in parentheses
-	// - Possible instances of conversation encapsulated in square brackets
-	// - Possible instances of words in square brackets that may be necessary for the sentence (i.e. need to have the brackets removed)
-	// - Possible instances of single quotes that should actually be double quotes (i.e. when a word is in single quotes, but is not inside of double quotes)
-	// `),
+	Example: heredoc.Doc(`Finds all translator's notes and moves them to their own file if present
+	epub-lint notes -f test.epub
+	`),
+	Long: heredoc.Doc(`Goes through all of the content files and looks for "TL Note:", "Translator's Note:", or "Note:"
+	and moves any matches to their own file with bidirectional linking between the footnote and its reference location.
+	It also adds an entry to the TOC and spine of the epub so the "tl_notes.xhtml" file is at the end of the file's contents.
+`),
 	Run: func(cmd *cobra.Command, args []string) {
 		err := validateCommonEpubFlags(epubFile)
 		if err != nil {
@@ -202,6 +170,15 @@ func moveTranslatorsNotes(epubFile string) error {
 
 				handledFiles = append(handledFiles, ncxFilePath)
 			}
+
+			var notesPluralization = "s"
+			if len(translatorNoteListItems) == 1 {
+				notesPluralization = ""
+			}
+			logger.WriteInfof("Found %d translator's note%s.\nAdding translator's notes file.\n", len(translatorNoteListItems), notesPluralization)
+
+		} else {
+			logger.WriteInfo("No translator's notes found.")
 		}
 
 		return handledFiles, nil
