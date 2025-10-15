d54423cce75873f5726a16a66e475ce9d3f4f9cb
Peter Kaufman <PeterJamesKaufman@gmail.com>
2025-10-14 20:03:32 -0400

    moved some of the logic away to its own location and started moving the logic for handling the validation errors as well

---
+++ epub-lint/internal/epub-check/constants.go
@@ -0,0 +1,13 @@
+package epubcheck
+
+const (
+	invalidIdPrefix         = "Error while parsing file: value of attribute \""
+	invalidAttribute        = "Error while parsing file: attribute \""
+	missingUniqueIdentifier = "The unique-identifier \""
+	emptyMetadataProperty   = "Error while parsing file: character content of element \""
+	invalidPlayOrder        = "Error while parsing file: identical playOrder values for navPoint/navTarget/pageTarget that do not refer to same target"
+	duplicateIdPrefix       = "Error while parsing file: Duplicate \""
+	invalidBlockquote       = "Error while parsing file: element \"blockquote\" incomplete;"
+	// jnovelsFile             = "jnovels.xhtml"
+	// jnovelsImage            = "1.png"
+)
