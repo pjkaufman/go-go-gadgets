75f7996160b469231da81184733801e75b377ad5
Peter Kaufman <PeterJamesKaufman@gmail.com>
2025-10-14 21:07:00 -0400

    Finished pulling out the logic for handling validation errors from epub check

--- epub-lint/cmd/fix_validation.go
+++ epub-lint/cmd/fix_validation.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/MakeNowJust/heredoc"
 	epubcheck "github.com/pjkaufman/go-go-gadgets/epub-lint/internal/epub-check"
-	rulefixes "github.com/pjkaufman/go-go-gadgets/epub-lint/internal/epub-check/rule-fixes"
 	epubhandler "github.com/pjkaufman/go-go-gadgets/epub-lint/internal/epub-handler"
 	filehandler "github.com/pjkaufman/go-go-gadgets/pkg/file-handler"
 	"github.com/pjkaufman/go-go-gadgets/pkg/logger"
@@ -91,8 +90,6 @@ var autoFixValidationCmd = &cobra.Command{
 
 		validationErrors.Sort()
 
-		var elementNameToNumber = make(map[string]int)
-
 		err = epubhandler.UpdateEpub(epubFile, func(zipFiles map[string]*zip.File, w *zip.Writer, epubInfo epubhandler.EpubInfo, opfFolder string) ([]string, error) {
 			var (
 				opfFilename = epubInfo.OpfFile
@@ -115,222 +112,27 @@ var autoFixValidationCmd = &cobra.Command{
 					ncxFilename: ncxFileContents,
 					opfFilename: opfFileContents,
 				}
-				handledFiles []string
+				handledFiles          []string
+				getFileContentsByName = func(filename string) (string, error) {
+					fileContents, ok := nameToUpdatedContents[filename]
+					if !ok {
+						zipFile, ok := zipFiles[filename]
+						if !ok {
+							return "", fmt.Errorf("failed to find %q in the epub", filename)
+						}
+
+						fileContents, err = filehandler.ReadInZipFileContents(zipFile)
+						if err != nil {
+							return "", err
+						}
+					}
+
+					return fileContents, nil
+				}
 			)
-			for i := 0; i < len(validationErrors.ValidationIssues); i++ {
-				message := validationErrors.ValidationIssues[i]
-
-				switch message.Code {
-				case "OPF-014":
-					nameToUpdatedContents[opfFilename], err = rulefixes.AddScriptedToManifest(nameToUpdatedContents[opfFilename], strings.TrimLeft(message.FilePath, opfFolder+"/"))
-
-					if err != nil {
-						return nil, err
-					}
-				case "OPF-015":
-					nameToUpdatedContents[opfFilename], err = rulefixes.RemoveScriptedFromManifest(nameToUpdatedContents[opfFilename], strings.TrimLeft(message.FilePath, opfFolder+"/"))
-
-					if err != nil {
-						return nil, err
-					}
-				case "NCX-001":
-					nameToUpdatedContents[opfFilename], err = rulefixes.FixIdentifierDiscrepancy(nameToUpdatedContents[opfFilename], nameToUpdatedContents[ncxFilename])
-
-					if err != nil {
-						return nil, err
-					}
-				case "RSC-005":
-					if strings.HasPrefix(message.Message, invalidIdPrefix) {
-						startIndex := strings.Index(message.Message, invalidIdPrefix)
-						if startIndex == -1 {
-							continue
-						}
-						startIndex += len(invalidIdPrefix)
-						endIndex := strings.Index(message.Message[startIndex:], `"`)
-						if endIndex == -1 {
-							continue
-						}
-
-						attribute := message.Message[startIndex : startIndex+endIndex]
-
-						// for now we will just fix the values in the opf and ncx files and we will handle the other cases separately
-						// when that is encountered since it requires keeping track of which files have already been modified
-						// and which ones have not been modified yet
-						if strings.HasSuffix(message.FilePath, ".opf") {
-							nameToUpdatedContents[opfFilename] = rulefixes.FixXmlIdValue(nameToUpdatedContents[opfFilename], message.Location.Line, attribute)
-						} else if strings.HasSuffix(message.FilePath, ".ncx") {
-							nameToUpdatedContents[ncxFilename] = rulefixes.FixXmlIdValue(nameToUpdatedContents[ncxFilename], message.Location.Line, attribute)
-						}
-					} else if strings.HasPrefix(message.Message, invalidAttribute) {
-						startIndex := strings.Index(message.Message, invalidAttribute)
-						if startIndex == -1 {
-							continue
-						}
-						startIndex += len(invalidAttribute)
-						endIndex := strings.Index(message.Message[startIndex:], `"`)
-						if endIndex == -1 {
-							continue
-						}
-
-						attribute := message.Message[startIndex : startIndex+endIndex]
-
-						// for now we will just fix the values in the opf file and we will handle the other cases separately
-						// when that is encountered since it requires keeping track of which files have already been modified
-						// and which ones have not been modified yet
-						if strings.HasSuffix(message.FilePath, ".opf") {
-							nameToUpdatedContents[opfFilename], err = rulefixes.FixManifestAttribute(nameToUpdatedContents[opfFilename], attribute, message.Location.Line-1, elementNameToNumber)
-							if err != nil {
-								return nil, err
-							}
-
-							validationErrors.IncrementLineNumbers(message.Location.Line, message.FilePath)
-						}
-					} else if strings.HasPrefix(message.Message, emptyMetadataProperty) {
-						startIndex := strings.Index(message.Message, emptyMetadataProperty)
-						if startIndex == -1 {
-							continue
-						}
-						startIndex += len(emptyMetadataProperty)
-						endIndex := strings.Index(message.Message[startIndex:], `"`)
-						if endIndex == -1 {
-							continue
-						}
-
-						elementName := message.Message[startIndex : startIndex+endIndex]
-
-						var deletedLine, oneDeleted bool
-						// for now we will just fix the values in the opf file and we will handle the other cases separately
-						// when that is encountered since it requires keeping track of which files have already been modified
-						// and which ones have not been modified yet
-						if strings.HasSuffix(message.FilePath, ".opf") {
-							nameToUpdatedContents[opfFilename], deletedLine, err = rulefixes.RemoveEmptyOpfElements(elementName, message.Location.Line-1, nameToUpdatedContents[opfFilename])
-							if err != nil {
-								return nil, err
-							}
-
-							if deletedLine {
-								validationErrors.DecrementLineNumbersAndRemoveLineReferences(message.Location.Line, message.FilePath)
-								oneDeleted = true
-							}
-						}
-
-						if oneDeleted {
-							i--
-						}
-					} else if message.Message == invalidPlayOrder {
-						nameToUpdatedContents[ncxFilename] = rulefixes.FixPlayOrder(nameToUpdatedContents[ncxFilename])
-					} else if strings.HasPrefix(message.Message, duplicateIdPrefix) {
-						startIndex := strings.Index(message.Message, duplicateIdPrefix)
-						if startIndex == -1 {
-							continue
-						}
-						startIndex += len(duplicateIdPrefix)
-						endIndex := strings.Index(message.Message[startIndex:], `"`)
-						if endIndex == -1 {
-							continue
-						}
-
-						id := message.Message[startIndex : startIndex+endIndex]
-
-						fileContents, ok := nameToUpdatedContents[message.FilePath]
-						if !ok {
-							zipFile, ok := zipFiles[message.FilePath]
-							if !ok {
-								return nil, fmt.Errorf("failed to find %q in the epub", message.FilePath)
-							}
-
-							fileContents, err = filehandler.ReadInZipFileContents(zipFile)
-							if err != nil {
-								return nil, err
-							}
-						}
-
-						fileContents, charactersAdded := rulefixes.UpdateDuplicateIds(fileContents, id)
-						nameToUpdatedContents[message.FilePath] = fileContents
-
-						if charactersAdded > 0 {
-							validationErrors.UpdateLineColumnPosition(message.Location.Line, message.Location.Column, charactersAdded, message.FilePath)
-						}
-					} else if strings.HasPrefix(message.Message, invalidBlockquote) {
-						fileContents, ok := nameToUpdatedContents[message.FilePath]
-						if !ok {
-							zipFile, ok := zipFiles[message.FilePath]
-							if !ok {
-								return nil, fmt.Errorf("failed to find %q in the epub", message.FilePath)
-							}
-
-							fileContents, err = filehandler.ReadInZipFileContents(zipFile)
-							if err != nil {
-								return nil, err
-							}
-						}
-
-						fileContents, charactersAdded := rulefixes.FixFailedBlockquoteParsing(message.Location.Line, message.Location.Column, fileContents)
-						nameToUpdatedContents[message.FilePath] = fileContents
-
-						if charactersAdded > 0 {
-							validationErrors.UpdateLineColumnPosition(message.Location.Line, message.Location.Column, charactersAdded, message.FilePath)
-						}
-					}
-				case "OPF-030":
-					startIndex := strings.Index(message.Message, missingUniqueIdentifier)
-					if startIndex == -1 {
-						continue
-					}
-					startIndex += len(missingUniqueIdentifier)
-					endIndex := strings.Index(message.Message[startIndex:], `"`)
-					if endIndex == -1 {
-						continue
-					}
-
-					nameToUpdatedContents[opfFilename], err = rulefixes.FixMissingUniqueIdentifierId(nameToUpdatedContents[opfFilename], message.Message[startIndex:startIndex+endIndex])
-					if err != nil {
-						return nil, err
-					}
-				case "RSC-012":
-					if strings.HasSuffix(message.FilePath, ".opf") {
-						nameToUpdatedContents[opfFilename] = rulefixes.RemoveLinkId(nameToUpdatedContents[opfFilename], message.Location.Line-1, message.Location.Column-1)
-					} else if strings.HasSuffix(message.FilePath, ".ncx") {
-						nameToUpdatedContents[ncxFilename] = rulefixes.RemoveLinkId(nameToUpdatedContents[ncxFilename], message.Location.Line-1, message.Location.Column-1)
-					} else {
-						if fileContents, ok := nameToUpdatedContents[message.FilePath]; ok {
-							nameToUpdatedContents[message.FilePath] = rulefixes.RemoveLinkId(fileContents, message.Location.Line-1, message.Location.Column-1)
-						} else {
-							zipFile, ok := zipFiles[message.FilePath]
-							if !ok {
-								return nil, fmt.Errorf("failed to find %q in the epub", message.FilePath)
-							}
-
-							fileContents, err := filehandler.ReadInZipFileContents(zipFile)
-							if err != nil {
-								return nil, err
-							}
-
-							nameToUpdatedContents[message.FilePath] = rulefixes.RemoveLinkId(fileContents, message.Location.Line-1, message.Location.Column-1)
-						}
-					}
-				}
-			}
-
-			if removeJNovelInfo {
-				// remove the jnovels file and the png associated with it
-				for file := range zipFiles {
-					// TODO: do I need to handle this better for 1-1.png as a scenario?
-					if strings.HasSuffix(file, jnovelsFile) || strings.HasSuffix(file, jnovelsImage) {
-						handledFiles = append(handledFiles, file)
-					}
-				}
-
-				// remove the associated files from the opf manifest and spine
-				nameToUpdatedContents[opfFilename], err = rulefixes.RemoveFileFromOpf(nameToUpdatedContents[opfFilename], jnovelsFile)
-				if err != nil {
-					return nil, err
-				}
-
-				nameToUpdatedContents[opfFilename], err = rulefixes.RemoveFileFromOpf(nameToUpdatedContents[opfFilename], jnovelsImage)
-				if err != nil {
-					return nil, err
-				}
+			err = epubcheck.HandleValidationErrors(opfFolder, ncxFilename, opfFileContents, nameToUpdatedContents, &validationErrors, getFileContentsByName)
+			if err != nil {
+				return nil, err
 			}
 
 			for filename, updatedContents := range nameToUpdatedContents {
