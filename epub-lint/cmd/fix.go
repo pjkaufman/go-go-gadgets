aae813c874d83caca906534bdc84555b6dd61298
Peter Kaufman <PeterJamesKaufman@gmail.com>
2025-10-24 17:05:24 -0400

    cleanup unused comments

--- epub-lint/cmd/fix.go
+++ epub-lint/cmd/fix.go
@@ -8,15 +8,6 @@ import (
 var fixCmd = &cobra.Command{
 	Use:   "fix",
 	Short: "Deals with fixing things with an epub file",
-	// Example: heredoc.Doc(`
-	// 	epub-lint fix-validation -f test.epub --issue-file epubCheckOutput.txt
-	// 	will read in the contents of the file and try to fix any of the fixable
-	// 	validation issues
-
-	// 	epub-lint fix-validation -f test.epub --issue-file epubCheckOutput.txt --cleanup-jnovels
-	// 	will read in the contents of the file and try to fix any of the fixable
-	// 	validation issues as well as remove any jnovels specific files
-	// `),
 }
 
 func init() {
