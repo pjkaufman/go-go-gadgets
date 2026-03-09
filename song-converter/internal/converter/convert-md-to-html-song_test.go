a86986d8fb87670e13733b6b4b2f463490332bad
Peter Kaufman <PeterJamesKaufman@gmail.com>
2026-03-09 15:30:22 -0400

    update UTs to no longer export types, variables, and properties that are just present for a specific test and don't get used outside of the test package

--- song-converter/internal/converter/convert-md-to-html-song_test.go
+++ song-converter/internal/converter/convert-md-to-html-song_test.go
@@ -9,42 +9,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type ConvertMdToHtmlSongTestCase struct {
-	InputFilePath string
-	InputContent  string
-	ExpectedHtml  string
+type convertMdToHtmlSongTestCase struct {
+	inputFilePath string
+	inputContent  string
+	expectedHtml  string
 }
 
-var ConvertMdToHtmlSongTestCases = map[string]ConvertMdToHtmlSongTestCase{
+var convertMdToHtmlSongTestCases = map[string]convertMdToHtmlSongTestCase{
 	"a valid file should properly get turned into html": {
-		InputFilePath: "He Is.md",
-		InputContent:  HeIsFileMd,
-		ExpectedHtml:  HeIsFileHtml,
+		inputFilePath: "He Is.md",
+		inputContent:  HeIsFileMd,
+		expectedHtml:  HeIsFileHtml,
 	},
 	"a valid file with another title should properly get converted into an html file": {
-		InputFilePath: "Above It All (There Stands Jesus).md",
-		InputContent:  AboveItAllFileMd,
-		ExpectedHtml:  AboveItAllFileHtml,
+		inputFilePath: "Above It All (There Stands Jesus).md",
+		inputContent:  AboveItAllFileMd,
+		expectedHtml:  AboveItAllFileHtml,
 	},
 	"a valid file with just the melody in the second row should properly get converted into an html file": {
-		InputFilePath: "Behold The Heavens.md",
-		InputContent:  BeholdTheHeavensFileMd,
-		ExpectedHtml:  BeholdTheHeavensFileHtml,
+		inputFilePath: "Behold The Heavens.md",
+		inputContent:  BeholdTheHeavensFileMd,
+		expectedHtml:  BeholdTheHeavensFileHtml,
 	},
 	"a valid file with a verse in the second row should properly get converted into an html file": {
-		InputFilePath: "Be Thou Exalted.md",
-		InputContent:  BeThouExaltedFileMd,
-		ExpectedHtml:  BeThouExaltedFileHtml,
+		inputFilePath: "Be Thou Exalted.md",
+		inputContent:  BeThouExaltedFileMd,
+		expectedHtml:  BeThouExaltedFileHtml,
 	},
 }
 
 func TestConvertMdToHtmlSong(t *testing.T) {
-	for name, args := range ConvertMdToHtmlSongTestCases {
+	for name, args := range convertMdToHtmlSongTestCases {
 		t.Run(name, func(t *testing.T) {
-			actual, err := converter.ConvertMdToHtmlSong(args.InputFilePath, args.InputContent, converter.Digital, false)
+			actual, err := converter.ConvertMdToHtmlSong(args.inputFilePath, args.inputContent, converter.Digital, false)
 			assert.Nil(t, err, "there should be no errors when parsing the song contents for the UTs")
 
-			assert.Equal(t, args.ExpectedHtml, actual)
+			assert.Equal(t, args.expectedHtml, actual)
 		})
 	}
 }
