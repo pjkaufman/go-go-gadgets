6dc9baa2700b710d63cce796f1c25d5969004b17
Peter Kaufman <PeterJamesKaufman@gmail.com>
2026-03-17 16:43:50 -0400

    added initial changes for fixing empty title elements

---
+++ epub-lint/internal/epub-check/rule-fixes/rsc-5-fix-empty-title.go
@@ -0,0 +1,89 @@
+package rulefixes
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/pjkaufman/go-go-gadgets/epub-lint/internal/epub-check/positions"
+)
+
+// FixEmptyTitle updates the title element's contents to be the first heading's text
+// or the first paragraph's text if there are no headings
+func FixEmptyTitle(line, column int, contents string) (edit positions.TextEdit) {
+	offset := positions.GetPositionOffset(contents, line, column)
+	if offset == -1 {
+		return
+	}
+
+	title := firstHeaderOrParagraphText(contents)
+	if strings.TrimSpace(title) == "" {
+		return
+	}
+
+	var (
+		newText       = title
+		startPosition = positions.Position{
+			Line:   line,
+			Column: column,
+		}
+		endPosition = startPosition
+	)
+	if contents[offset-2] == '/' { // we are dealing with a self-closing title
+		startIndex := strings.LastIndex(contents[:offset], "<")
+		if startIndex == -1 { // something went wrong, so we will skip this one
+			return
+		}
+
+		startPosition = positions.IndexToPosition(contents, startIndex)
+		newText = fmt.Sprintf("<title>%s</title>", title)
+	}
+
+	edit.Range.Start = startPosition
+	edit.Range.End = endPosition
+	edit.NewText = newText
+
+	return
+}
+
+type textNode struct {
+	Text string `xml:",chardata"`
+}
+
+func firstHeaderOrParagraphText(contents string) string {
+	var (
+		isFirstParagraph   = true
+		firstParagraphText string
+		decoder            = xml.NewDecoder(strings.NewReader(contents))
+	)
+
+	for {
+		tok, err := decoder.Token()
+		if err != nil {
+			if err == io.EOF {
+				return firstParagraphText
+			}
+
+			return ""
+		}
+
+		switch se := tok.(type) {
+		case xml.StartElement:
+			if se.Name.Local == "h1" || se.Name.Local == "h2" ||
+				se.Name.Local == "h3" || se.Name.Local == "h4" ||
+				se.Name.Local == "h5" || se.Name.Local == "h6" {
+
+				var h textNode
+				decoder.DecodeElement(&h, &se)
+				return strings.TrimSpace(h.Text)
+			} else if se.Name.Local == "p" && isFirstParagraph {
+				var h textNode
+				decoder.DecodeElement(&h, &se)
+
+				firstParagraphText = strings.TrimSpace(h.Text)
+				isFirstParagraph = false
+			}
+		}
+	}
+}
---
+++ epub-lint/internal/epub-check/rule-fixes/rsc-5-fix-empty-title_test.go
@@ -0,0 +1,136 @@
+//go:build unit
+
+package rulefixes_test
+
+import (
+	"testing"
+
+	rulefixes "github.com/pjkaufman/go-go-gadgets/epub-lint/internal/epub-check/rule-fixes"
+)
+
+type FixEmptyTitleTestCase struct {
+	input        string
+	line, column int
+	expected     string
+}
+
+var fixEmptyTitleTestCases = map[string]FixEmptyTitleTestCase{
+	"An empty title element should get properly filled in by the first heading in the file": {
+		input: `<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <title></title>
+  </head>
+  <body>
+  <h1>Table of Contents</h1>
+  <h2>Subtitle</h2>
+  </body>
+</html>`,
+		line:   6,
+		column: 10,
+		expected: `<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <title>Table of Contents</title>
+  </head>
+  <body>
+  <h1>Table of Contents</h1>
+  <h2>Subtitle</h2>
+  </body>
+</html>`,
+	},
+	"An empty self-closing title element should get properly filled in by the first heading in the file": {
+		input: `<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <title/>
+  </head>
+  <body>
+  <h4>Table of Contents</h4>
+  <h5>Subtitle</h5>
+  </body>
+</html>`,
+		line:   6,
+		column: 11,
+		expected: `<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <title>Table of Contents</title>
+  </head>
+  <body>
+  <h4>Table of Contents</h4>
+  <h5>Subtitle</h5>
+  </body>
+</html>`,
+	},
+	"An empty title element should get filled in with the contents of the first paragraph when no heading element is present": {
+		input: `<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <title/>
+  </head>
+  <body>
+  <p>Paragraph 1</p>
+  <p>Paragraph 2</p>
+  </body>
+</html>`,
+		line:   6,
+		column: 11,
+		expected: `<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <title>Paragraph 1</title>
+  </head>
+  <body>
+  <p>Paragraph 1</p>
+  <p>Paragraph 2</p>
+  </body>
+</html>`,
+	},
+	"An empty title element should be left as is when no heading or paragraph element is present": {
+		input: `<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <title/>
+  </head>
+  <body>
+  </body>
+</html>`,
+		line:   6,
+		column: 11,
+		expected: `<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <title/>
+  </head>
+  <body>
+  </body>
+</html>`,
+	},
+}
+
+func TestFixEmptyTitle(t *testing.T) {
+	for name, args := range fixEmptyTitleTestCases {
+		t.Run(name, func(t *testing.T) {
+			edit := rulefixes.FixEmptyTitle(args.line, args.column, args.input)
+
+			checkFinalOutputMatches(t, args.input, args.expected, edit)
+		})
+	}
+}
