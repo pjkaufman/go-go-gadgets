6d09c78fb0f48b06feec0b78d6f7c903c4f25c23
Peter Kaufman <PeterJamesKaufman@gmail.com>
2026-03-30 17:25:22 -0400

    added the base line logic for the refactored move translator's notes, but I still need to add several UTs and wire things up still

---
+++ epub-lint/internal/epub-handler/move-translators-notes.go
@@ -0,0 +1,213 @@
+package epubhandler
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/pjkaufman/go-go-gadgets/epub-lint/internal/linter"
+)
+
+const tlNoteFileName = "tl_notes.xhtml"
+
+var defaultTLNoteContents = `<?xml version='1.0' encoding='utf-8'?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Translator's Notes</title>
+</head>
+<body>
+    <h3>Translator's Notes</h3>
+    <ol>
+				%s</ol>
+</body>
+</html>
+`
+
+func MoveTranslatorsNotes(spineOrder []string, opfFolder, ncxFilename, opfFilename string, nameToUpdatedContents map[string]string, getContentByFileName func(string) (string, error)) (int, error) {
+	var (
+		translatorNoteListItems []string
+		fileTranslatorNotes     []string
+		startingNumber          int
+		fullFilePath            string
+	)
+	for _, file := range spineOrder {
+		fullFilePath = filepath.Join(opfFolder, file)
+
+		contents, err := getContentByFileName(fullFilePath)
+		if err != nil {
+			return 0, err
+		}
+
+		var nameParts = strings.Split(file, "/")
+		contents, fileTranslatorNotes, startingNumber = linter.GetTranslatorsNotes(contents, nameParts[len(nameParts)-1], tlNoteFileName, startingNumber)
+		translatorNoteListItems = append(translatorNoteListItems, fileTranslatorNotes...)
+
+		nameToUpdatedContents[fullFilePath] = contents
+	}
+
+	/*TODO:
+	If there are translator's notes:
+	- Add tl_notes.xhtml to the nav
+	*/
+	if len(translatorNoteListItems) > 0 {
+		if opfFolder == "." {
+			opfFolder = ""
+		}
+
+		var (
+			pathParts      = strings.Split(fullFilePath, "/")
+			htmlFolderPath = opfFolder
+			relativePath   = tlNoteFileName
+		)
+		if len(pathParts) > 1 {
+			htmlFolderPath = strings.Join(pathParts[0:len(pathParts)-1], "/")
+		}
+
+		if len(pathParts) > 2 {
+			relativePath = filepath.Join(strings.Join(pathParts[1:len(pathParts)-1], "/"), tlNoteFileName)
+		}
+
+		var tlNotesFilePath = tlNoteFileName
+		if htmlFolderPath != "" {
+			tlNotesFilePath = filepath.Join(htmlFolderPath, tlNoteFileName)
+		}
+
+		nameToUpdatedContents[tlNotesFilePath] = fmt.Sprintf(defaultTLNoteContents, strings.Join(translatorNoteListItems, "				"))
+
+		opfFileContents, err := getContentByFileName(opfFilename)
+		if err != nil {
+			return 0, err
+		}
+
+		if opfFolder == "" && relativePath == tlNoteFileName {
+			relativePath = tlNotesFilePath
+		}
+		opfFileContents = AddFileToOpf(opfFileContents, relativePath, "tl_notes", "application/xhtml+xml")
+		nameToUpdatedContents[opfFilename] = opfFileContents
+
+		if ncxFilename != "" {
+			ncxFileContents, err := getContentByFileName(ncxFilename)
+			if err != nil {
+				return 0, err
+			}
+
+			ncxFileContents = AddFileToNcx(ncxFileContents, relativePath, "Translator's Notes", "tl_notes")
+			nameToUpdatedContents[ncxFilename] = ncxFileContents
+		}
+	}
+
+	return len(translatorNoteListItems), nil
+
+	// return epubhandler.UpdateEpub(epubFile, func(zipFiles map[string]*zip.File, w *zip.Writer, epubInfo epubhandler.EpubInfo, opfFolder string) ([]string, error) {
+	// 	var (
+	// 		handledFiles            []string
+	// 		translatorNoteListItems []string
+	// 		fileTranslatorNotes     []string
+	// 		startingNumber          int
+	// 		fullFilePath            string
+	// 	)
+	// 	for _, file := range epubInfo.FilePathsInSpineOrder {
+	// 		if _, ok := epubInfo.HtmlFiles[file]; !ok {
+	// 			continue
+	// 		}
+
+	// 		var (
+	// 			filePath = getFilePath(opfFolder, file)
+	// 			zipFile  = zipFiles[filePath]
+	// 		)
+
+	// 		fullFilePath = filePath
+
+	// 		fileText, err := filehandler.ReadInZipFileContents(zipFile)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+
+	// var nameParts = strings.Split(file, "/")
+	// fileText, fileTranslatorNotes, startingNumber = linter.GetTranslatorsNotes(fileText, nameParts[len(nameParts)-1], tlNoteFileName, startingNumber)
+
+	// 		translatorNoteListItems = append(translatorNoteListItems, fileTranslatorNotes...)
+	// 	}
+
+	// /*TODO:
+	// If there are translator's notes:
+	// - Add tl_notes.xhtml to the nav
+	// */
+	// if len(translatorNoteListItems) > 0 {
+	// 	if opfFolder == "." {
+	// 		opfFolder = ""
+	// 	}
+
+	// 	var (
+	// 		pathParts      = strings.Split(fullFilePath, "/")
+	// 		htmlFolderPath = opfFolder
+	// 		relativePath   = tlNoteFileName
+	// 	)
+	// 	if len(pathParts) > 1 {
+	// 		htmlFolderPath = strings.Join(pathParts[0:len(pathParts)-1], "/")
+	// 	}
+
+	// 	if len(pathParts) > 2 {
+	// 		relativePath = getFilePath(strings.Join(pathParts[1:len(pathParts)-1], "/"), tlNoteFileName)
+	// 	}
+
+	// 	var tlNotesFilePath = tlNoteFileName
+	// 	if htmlFolderPath != "" {
+	// 		tlNotesFilePath = getFilePath(htmlFolderPath, tlNoteFileName)
+	// 	}
+
+	// 	err = filehandler.WriteZipCompressedString(w, tlNotesFilePath, fmt.Sprintf(defaultTLNoteContents, strings.Join(translatorNoteListItems, "				")))
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+
+	// 	handledFiles = append(handledFiles, tlNotesFilePath)
+
+	// 	opfFile := zipFiles[epubInfo.OpfFile]
+	// 	opfFileContents, err := filehandler.ReadInZipFileContents(opfFile)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+
+	// 	if opfFolder == "" && relativePath == tlNoteFileName {
+	// 		relativePath = tlNotesFilePath
+	// 	}
+	// 	opfFileContents = epubhandler.AddFileToOpf(opfFileContents, relativePath, "tl_notes", "application/xhtml+xml")
+	// 	err = filehandler.WriteZipCompressedString(w, epubInfo.OpfFile, opfFileContents)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+
+	// 	handledFiles = append(handledFiles, epubInfo.OpfFile)
+
+	// 	if epubInfo.NcxFile != "" {
+	// 		var ncxFilePath = getFilePath(opfFolder, epubInfo.NcxFile)
+	// 		ncxFileContents, err := filehandler.ReadInZipFileContents(zipFiles[ncxFilePath])
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+
+	// 		ncxFileContents = epubhandler.AddFileToNcx(ncxFileContents, relativePath, "Translator's Notes", "tl_notes")
+
+	// 		err = filehandler.WriteZipCompressedString(w, ncxFilePath, ncxFileContents)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+
+	// 			handledFiles = append(handledFiles, ncxFilePath)
+	// 		}
+
+	// 		var notesPluralization = "s"
+	// 		if len(translatorNoteListItems) == 1 {
+	// 			notesPluralization = ""
+	// 		}
+	// 		logger.WriteInfof("Found %d translator's note%s.\nAdding translator's notes file.\n", len(translatorNoteListItems), notesPluralization)
+
+	// 	} else {
+	// 		logger.WriteInfo("No translator's notes found.")
+	// 	}
+
+	// 	return handledFiles, nil
+	// })
+	// return 0, nil
+}
---
+++ epub-lint/internal/epub-handler/move-translators-notes_test.go
@@ -0,0 +1,121 @@
+//go:build unit
+
+package epubhandler_test
+
+import (
+	"fmt"
+	"testing"
+
+	_ "embed"
+
+	epubhandler "github.com/pjkaufman/go-go-gadgets/epub-lint/internal/epub-handler"
+	"github.com/stretchr/testify/assert"
+)
+
+type moveTranslatorsNotesTestCase struct {
+	opfFolder, ncxFilename, opfFilename           string
+	expectedTranslatorNoteCount                   int
+	expectedFileState, validFilesToInitialContent map[string]string // filename to content
+	spineOrder                                    []string
+}
+
+var (
+	//go:embed testdata/move-translators-notes/simple-html-file-no-translators-notes.xhtml
+	noTranslatorNotesXhtml string
+	//go:embed testdata/move-translators-notes/html-file-with-single-translators-note.html
+	htmlSingleTranslatorNoteOriginal string
+	//go:embed testdata/move-translators-notes/html-file-with-single-translators-note_updated.html
+	htmlSingleTranslatorNoteExpected string
+	//go:embed testdata/move-translators-notes/simple.opf
+	opfSimpleOriginal string
+	//go:embed testdata/move-translators-notes/simple_updated.opf
+	opfSimpleExpected string
+	//go:embed testdata/move-translators-notes/simple.ncx
+	ncxSimpleOriginal string
+	//go:embed testdata/move-translators-notes/simple_updated.ncx
+	ncxSimpleExpected string
+	//go:embed testdata/move-translators-notes/translators-notes-simple.xhtml
+	xhtmlSimpleTranslatorsNotes string
+)
+
+var moveTranslatorsNotesTestCases = map[string]moveTranslatorsNotesTestCase{
+	"When no translator's notes are present in any files, no changes are made": {
+		opfFolder:   "OPS",
+		ncxFilename: "OPS/toc.ncx",
+		opfFilename: "OPS/content.opf",
+		spineOrder: []string{
+			"Text/section-0001.html",
+			"Text/section-0002.html",
+		},
+		expectedTranslatorNoteCount: 0,
+		expectedFileState: map[string]string{
+			"OPS/Text/section-0001.html": noTranslatorNotesXhtml,
+			"OPS/Text/section-0002.html": noTranslatorNotesXhtml,
+		},
+		validFilesToInitialContent: map[string]string{
+			"OPS/Text/section-0001.html": noTranslatorNotesXhtml,
+			"OPS/Text/section-0002.html": noTranslatorNotesXhtml,
+		},
+	},
+	"When a single translator's note is found, then it should be replaced with a reference and the corresponding changes should be made to the OPF and NCX files": {
+		opfFolder:   "OPS",
+		ncxFilename: "OPS/toc.ncx",
+		opfFilename: "OPS/content.opf",
+		spineOrder: []string{
+			"Text/section-0001.html",
+			"Text/section-0002.html",
+		},
+		expectedTranslatorNoteCount: 1,
+		expectedFileState: map[string]string{
+			"OPS/Text/section-0001.html": noTranslatorNotesXhtml,
+			"OPS/Text/section-0002.html": htmlSingleTranslatorNoteExpected,
+			"OPS/toc.ncx":                ncxSimpleExpected,
+			"OPS/content.opf":            opfSimpleExpected,
+			"OPS/Text/tl_notes.xhtml":    xhtmlSimpleTranslatorsNotes,
+		},
+		validFilesToInitialContent: map[string]string{
+			"OPS/Text/section-0001.html": noTranslatorNotesXhtml,
+			"OPS/Text/section-0002.html": htmlSingleTranslatorNoteOriginal,
+			"OPS/toc.ncx":                ncxSimpleOriginal,
+			"OPS/content.opf":            opfSimpleOriginal,
+		},
+	},
+	/* Test cases to add:
+	3. Multiple translator's notes present get replaced with the proper references and corresponding tl_notes file is created
+	4. No OPF folder (i.e. the OPF file is at the base of the zip) when translator's notes are found generates the correct paths and content for the tl_notes, NCX, and OPF
+	5. Current bug scenario gets found and fixed (see #88)
+	6. See about the scenario where all of the body content is inside a div and there is a translator's note
+	7. Make sure that it handles partial element translator's notes correctly
+	8. Make sure that it handles full element translator's notes correctly
+	*/
+}
+
+func createTestCaseFileHandlerFunction(validFilesToContent map[string]string, currentContents map[string]string) func(string) (string, error) {
+	return func(s string) (string, error) {
+		if content, ok := currentContents[s]; ok {
+			return content, nil
+		} else if content, ok := validFilesToContent[s]; ok {
+			return content, nil
+		}
+
+		return "", fmt.Errorf("unexpected attempt to get file contents for file %q", s)
+	}
+}
+
+func TestMoveTranslatorsNotes(t *testing.T) {
+	for name, tc := range moveTranslatorsNotesTestCases {
+		t.Run(name, func(t *testing.T) {
+			var nameToUpdatedFileContents = map[string]string{}
+
+			actualTranslatorNoteCount, err := epubhandler.MoveTranslatorsNotes(tc.spineOrder, tc.opfFolder, tc.ncxFilename, tc.opfFilename, nameToUpdatedFileContents, createTestCaseFileHandlerFunction(tc.validFilesToInitialContent, nameToUpdatedFileContents))
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expectedTranslatorNoteCount, actualTranslatorNoteCount)
+
+			for name, expectedContents := range tc.expectedFileState {
+				actualContents, ok := nameToUpdatedFileContents[name]
+				assert.True(t, ok, fmt.Sprintf("expected file %q to be updated, but it was not", name))
+				assert.Equal(t, expectedContents, actualContents, fmt.Sprintf("expected file contents for %q did not match actual contents", name))
+			}
+		})
+	}
+}
