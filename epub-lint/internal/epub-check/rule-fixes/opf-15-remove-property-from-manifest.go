c511fa9b21b7b660f6e3a59b69470885b2eb273c
Peter Kaufman <PeterJamesKaufman@gmail.com>
2026-04-04 20:13:44 -0400

    added extract id usage to remove property from manifest

--- epub-lint/internal/epub-check/rule-fixes/opf-15-remove-property-from-manifest.go
+++ epub-lint/internal/epub-check/rule-fixes/opf-15-remove-property-from-manifest.go
@@ -9,8 +9,10 @@ import (
 )
 
 func RemovePropertyFromManifest(opfContents, fileName, property string) (positions.TextEdit, error) {
-	var edit positions.TextEdit
-	startIndex, _, manifestContent, err := epubhandler.GetManifestContents(opfContents)
+	var (
+		edit                                positions.TextEdit
+		startIndex, _, manifestContent, err = epubhandler.GetManifestContents(opfContents)
+	)
 	if err != nil {
 		return edit, err
 	}
@@ -34,44 +36,35 @@ func RemovePropertyFromManifest(opfContents, fileName, property string) (positio
 	}
 
 	var (
-		propertiesAttr         = `properties="`
-		element                = manifestContent[startOfElement : startOfHref+endOfElement]
-		startOfPropertiesIndex = strings.Index(element, propertiesAttr)
+		propertiesAttr                                   = "properties"
+		element                                          = manifestContent[startOfElement : startOfHref+endOfElement]
+		properties                                       string
+		startOfAttributeValue, attributeEnd              int
+		removePropertiesStartPos, removePropertiesEndPos positions.Position
 	)
 
-	var removePropertiesStartPos, removePropertiesEndPos positions.Position
-	if startOfPropertiesIndex != -1 {
-		var startOfAttributeValue = startOfPropertiesIndex + len(propertiesAttr)
-		if element[startOfAttributeValue] == '"' {
+	properties, startOfAttributeValue, attributeEnd, err = epubhandler.ExtractAttribute(element, propertiesAttr)
+	if err != nil {
+		return edit, nil // not found so we can ignore it
+	}
+
+	var startOfValueIndex = startIndex + startOfElement + startOfAttributeValue
+	if strings.TrimSpace(properties) == property {
+		// remove properties attribute and the preceding space
+		removePropertiesStartPos = positions.IndexToPosition(opfContents, startIndex+startOfElement+startOfAttributeValue-len(propertiesAttr)-3) // account for attribute name, "=", and opening quote
+		removePropertiesEndPos = positions.IndexToPosition(opfContents, startIndex+startOfElement+attributeEnd+1)
+	} else {
+		propertyIndex := strings.Index(properties, property)
+		if propertyIndex == -1 {
 			return edit, nil
 		}
 
-		attributeEnd := strings.Index(element[startOfAttributeValue:], "\"")
-		if attributeEnd == -1 {
-			return edit, nil
-		}
-
-		var (
-			attributeValue    = element[startOfAttributeValue : startOfAttributeValue+attributeEnd]
-			startOfValueIndex = startIndex + startOfElement + startOfAttributeValue
-		)
-		if strings.TrimSpace(attributeValue) == property {
-			// remove properties attribute and the preceding space
-			removePropertiesStartPos = positions.IndexToPosition(opfContents, startIndex+startOfElement+startOfPropertiesIndex-1)
-			removePropertiesEndPos = positions.IndexToPosition(opfContents, startIndex+startOfElement+startOfAttributeValue+attributeEnd+1)
+		if propertyIndex == 0 {
+			removePropertiesStartPos = positions.IndexToPosition(opfContents, startOfValueIndex)
+			removePropertiesEndPos = positions.IndexToPosition(opfContents, startOfValueIndex+len(property)+1) // remove the following space
 		} else {
-			propertyIndex := strings.Index(attributeValue, property)
-			if propertyIndex == -1 {
-				return edit, nil
-			}
-
-			if propertyIndex == 0 {
-				removePropertiesStartPos = positions.IndexToPosition(opfContents, startOfValueIndex)
-				removePropertiesEndPos = positions.IndexToPosition(opfContents, startOfValueIndex+len(property)+1) // remove the following space
-			} else {
-				removePropertiesStartPos = positions.IndexToPosition(opfContents, startOfValueIndex+propertyIndex-1) // remove the preceding space
-				removePropertiesEndPos = positions.IndexToPosition(opfContents, startOfValueIndex+propertyIndex+len(property))
-			}
+			removePropertiesStartPos = positions.IndexToPosition(opfContents, startOfValueIndex+propertyIndex-1) // remove the preceding space
+			removePropertiesEndPos = positions.IndexToPosition(opfContents, startOfValueIndex+propertyIndex+len(property))
 		}
 	}
 
