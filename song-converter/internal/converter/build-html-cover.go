19e80e30ad75f25d8418117e82c875be2c21eaf6
Peter Kaufman <PeterJamesKaufman@gmail.com>
2026-08-01 21:00:26 -0400

    better cover handling

--- song-converter/internal/converter/build-html-cover.go
+++ song-converter/internal/converter/build-html-cover.go
@@ -15,7 +15,9 @@ func BuildHtmlCover(coverMd, bookType, extraStyleCss string, currentTime time.Ti
 	coverMd = strings.Replace(coverMd, "{{TYPE}}", bookType, 1)
 
 	coverHtml := mdToHTML([]byte(coverMd))
+	// TODO: allow specifying left for the text alignment for the book type
 	coverHtml = fmt.Sprintf("<div style=\"text-align: center%s\">\n%s</div>\n", extraStyleCss, coverHtml)
+	coverHtml = strings.ReplaceAll(coverHtml, "&amp;nbsp;", "&nbsp;")
 
 	return strings.ReplaceAll(coverHtml, "\n\n", "\n")
 }
