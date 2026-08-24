c14ea45086b3427c5b4333c0ec629011147d89b2
Peter Kaufman <PeterJamesKaufman@gmail.com>
2026-08-23 22:10:25 -0400

    remove dead code

--- song-converter/internal/converter/filter-and-sort-songs.go
+++ song-converter/internal/converter/filter-and-sort-songs.go
@@ -15,48 +15,6 @@ type filterData struct {
 
 var h1Regex = regexp.MustCompile("\n#\\s+(.+)\n")
 
-func FilterAndSortSongs(mdInfo []MdFileInfo, location string) ([]MdFileInfo, error) {
-	var pageInfo = make([]filterData, 0, len(mdInfo))
-	for _, mdData := range mdInfo {
-		var metadata SongMetadata
-		_, err := parseFrontmatter(mdData.FilePath, mdData.FileContents, &metadata)
-		if err != nil {
-			return nil, err
-		}
-
-		pageNumbers := getPageNumbers(location, metadata.BookLocation)
-		if len(pageNumbers) == 0 {
-			continue
-		}
-
-		mdData.PrimaryPageNumbers = pageNumbers
-		mdData.Header = getHeaderText(mdData.FileContents)
-
-		for _, pageNumber := range pageNumbers {
-			pageInfo = append(pageInfo, filterData{
-				mdInfo:     &mdData,
-				pageNumber: pageNumber,
-			})
-		}
-	}
-
-	c := collate.New(language.English)
-	sort.Slice(pageInfo, func(i, j int) bool {
-		if pageInfo[i].pageNumber != pageInfo[j].pageNumber {
-			return pageInfo[i].pageNumber < pageInfo[j].pageNumber
-		}
-
-		return c.CompareString(pageInfo[i].mdInfo.FileName, pageInfo[j].mdInfo.FileName) < 0
-	})
-
-	var newMdInfo = make([]MdFileInfo, len(pageInfo))
-	for i, pageData := range pageInfo {
-		newMdInfo[i] = *pageData.mdInfo
-	}
-
-	return newMdInfo, nil
-}
-
 func SortSongs(mdInfo []MdFileInfo) []MdFileInfo {
 	var pageInfo = make([]filterData, 0, len(mdInfo))
 	for _, mdData := range mdInfo {
